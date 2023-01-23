4a8102d213e611c13276f6b2e4cfc74466e8bc0f
Owen Johnson <owenbjohnson10@gmail.com>
2023-01-23 09:51:55 -0600

    Set up list edit functionality

--- To Do List/ContentView.swift
+++ To Do List/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var things = ["First Thing", "Second Thing", "Third Thing", "Fourth Thing", "Fifth Thing"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(things, id: \.self) { thing in
+                    Text(thing)
+                }
+                .onMove { indices, newOffset, in
+                    things.move(fromOffsets: indices, toOffset: newOffset)
+                }
+                .onDelete { indexSet in
+                    things.remove(atOffsets: indexSet)
+                }
+            }
+            .navigationBarTitle("Things", displayMode: .inline)
+            .navigationBarItems(leading: EditButton())
         }
-        .padding()
     }
 }
 
