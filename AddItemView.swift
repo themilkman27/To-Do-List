5e88955c5d4b55f48ff5a49ff55cc237f03e6db6
Owen Johnson <owenbjohnson10@gmail.com>
2023-02-05 18:04:39 -0600

    Created Add Items view

---
+++ AddItemView.swift
@@ -0,0 +1,37 @@
+//
+//  AddItemView.swift
+//  To Do List
+//
+//  Created by Owen Johnson on 2/5/23.
+//
+
+import SwiftUI
+
+struct AddItemView: View {
+    @ObservedObject var toDoList : ToDoList
+    @State private var priority = ""
+    @State private var description = ""
+    @State private var dueDate = Date()
+    @Environment(\.presentationMode) var presentationMode
+    static let priorities = ["High", "Medium", "Low"]
+
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("Priority", selection: $priority) {
+                    ForEach(Self.priorities, id: \.self) { priority in
+                        Text(priority)
+                        
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct AddItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddItemView(toDoList: ToDoList())
+    }
+}
